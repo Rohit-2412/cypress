7f12185fe77230ee4e6f2098cd163970e30e4a9f
Rohit <rk212300@outlook.com>
2023-12-07 18:00:45 +0530

    landing page, auth done

--- src/lib/providers/next-theme-provider.tsx
+++ src/lib/providers/next-theme-provider.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
 
-export const NextThemeProvider = ({
+export const ThemeProvider = ({
     children,
     ...props
 }: ThemeProviderProps) => {
